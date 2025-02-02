fa428af5387411873a47ba4feb0a5a296b531c20
knm22mukk <knm22mukk@gmail.com>
2025-02-02 11:31:58 +0900

    フォーマットを再実行

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,8 @@ import TestComponent from '@/components/TestComponent';
 export default function Home() {
   return (
     <div>
-      <h1 className='text-red-500 text-4xl font-extrabold p-20'>
-        next.jsテスト
-      </h1>
-      <TestComponent />
+      <h1 className='text-red-500 text-4xl font-extrabold p-20'>next.js</h1>
+      <TestComponent content='lefthookが正しく動くように変更' />
     </div>
   );
 }
